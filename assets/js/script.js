168a4ff4a7a5e8ff28322c48dd67df5f5cba5249
antunahuel <dgarridoantinao@gmail.com>
2024-03-05 16:37:08 -0300

    se agrego cdn  bootstra más otro enlacep

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
 $(function(){
 // var titulo=$("h1");
 // console.log(titulo);
